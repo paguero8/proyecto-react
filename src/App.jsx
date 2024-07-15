8dce786d5cec22183f7e89462ee16d99710bc6a1
Santiago dos Santos <santiiagodossantos@icloud.com>
2024-07-15 20:37:18 -0300

    API Call

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
+import { useEffect } from 'react'
 import './App.css'
 import { Movie } from './components/Movies'
 
 function App() {
 
+  useEffect(() => {
+    getMovies()
+  }
+  , [])
+
+  const getMovies =  () => {
+    fetch('https://www.omdbapi.com/?apikey=9d243264&t=avengers')
+      .then(response => response.json())
+      .then(data=> console.log(data))
+  }
+
   return (
-    <>
-     <Movie title="The Godfather"/>
-    </>
+    <div className="page">
+      <header>
+        <h2>Buscar película</h2>
+        <div className="search">
+          <input type='text' placeholder='Titanic, Shrek...'></input>
+          <button type='submit'>Buscar</button>
+        </div>
+      </header>
+
+      <main>
+        <Movie title="The Godfather" poster="" />
+      </main>
+    </div>
+    
   )
 }
 
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,12 +1,11 @@
 /* eslint-disable react/prop-types */
 
-const Movie = ({title}) => {
+const Movie = ({title, poster}) => {
     return(
         <div>
-            <img src="https://via.placeholder.com/150" alt="movie"/>
+            <img src={poster} alt="movie"/>
             <h3>{title}</h3>
         </div>
-
     )
 }
 
